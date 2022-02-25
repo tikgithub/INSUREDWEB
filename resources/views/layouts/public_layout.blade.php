3906db905d40ebcf3abbe9b78ccab0ace3b3eb5b
tikgithub <tik.github@gmail.com>
2022-02-25 17:29:54 +0700

    Update

--- resources/views/layouts/public_layout.blade.php
+++ resources/views/layouts/public_layout.blade.php
@@ -114,6 +114,9 @@
     <div class="container">
         @yield('content')
     </div>
+    <div class="">
+        @yield('footer')
+    </div>
     {{-- End NavBar --}}
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
         integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.public_layout')
 @section('nav-content')
 <div class="pt-4" style="margin-top: 10px;"></div>
-
+{{-- Carousel Section --}}
 <div class="">
     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
@@ -35,7 +35,106 @@
         </button>
     </div>
 </div>
+{{-- End Carousel Section --}}
 @endsection
 @section('content')
+{{-- Padding --}}
+<div class="pt-3"></div>
+{{-- ********************************************************************* --}}
+<div class="row">
+    <div class="col-md-12 text-center notosanLao">
+        <h3>ຮູບແບບປະກັນໄພ</h3>
+    </div>
+</div>
+{{-- padding --}}
+<div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="car">
+        <div class="card-body">
+            <a href="http://">
+                <img src="{{asset('assets/image/example1.jpeg')}}" class="card-img-top shadow" alt="...">
+            </a>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+        <div class="car">
+          <div class="card-body">
+              <a href="http://">
+                  <img src="{{asset('assets/image/example1.jpeg')}}" class="card-img-top shadow" alt="...">
+              </a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="car">
+          <div class="card-body">
+              <a href="http://">
+                  <img src="{{asset('assets/image/example1.jpeg')}}" class="card-img-top shadow" alt="...">
+              </a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="car">
+          <div class="card-body">
+              <a href="http://">
+                  <img src="{{asset('assets/image/example1.jpeg')}}" class="card-img-top shadow" alt="...">
+              </a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="car">
+          <div class="card-body">
+              <a href="http://">
+                  <img src="{{asset('assets/image/example1.jpeg')}}" class="card-img-top shadow" alt="...">
+              </a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="car">
+          <div class="card-body">
+              <a href="http://">
+                  <img src="{{asset('assets/image/example1.jpeg')}}" class="card-img-top shadow" alt="...">
+              </a>
+          </div>
+        </div>
+      </div>
+
+  </div>
+{{-- ********************************************************************* --}}
+
+
+{{-- ****************************** HOW TO PAY *************************** --}}
+<div class="row">
+    <div class="col-md-12">
+        <a href="http://">
+            <img src="{{asset('assets/image/example1.jpeg')}}" class="card-img-top shadow" alt="...">
+        </a>
+    </div>
+</div>
+{{-- ********************************************************************* --}}
+@endsection
+
+@section('footer')
+<nav class=" bg-blue" style="margin-bottom: -150px;" >
+    <div class="container-fluid">
+        <div class="row" style="padding-bottom: 100px;">
+            <div class="col-md-12 pt-2 d-flex justify-content-end align-self-center">
+                <span class="col-md-6">
+                    <h4 class="text-white">{{config("app.name")}}</h4>
+                </span>
+                <span class="col-md-6 d-flex justify-content-end align-self-center">
+                    <img class="ms-2" src="{{asset('assets/image/facebook.png')}}" style="width: auto;height: 30px;">
+                    <img class="ms-2" src="{{asset('assets/image/instragram.png')}}" style="width: auto;height: 30px;">
+                    <img class="ms-2"  src="{{asset('assets/image/linkined.png')}}" style="width: auto;height: 30px;">
+                </span>
+            </div>
+
+        </div>
+    </div>
+</nav>
 
 @endsection
