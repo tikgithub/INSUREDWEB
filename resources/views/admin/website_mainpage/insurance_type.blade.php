dc6f8c096f020b02594433ef07b11ea462f0ca41
TikGithub <tikgithub@gmail.com>
2022-04-26 10:00:03 +0700

    Update insurance_type.blade.php

--- resources/views/admin/website_mainpage/insurance_type.blade.php
+++ resources/views/admin/website_mainpage/insurance_type.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin_layout')
 @section('content')
-<div class="pt-5"></div>
-<div class="pt-5"></div>
+    <div class="pt-5"></div>
+    <div class="pt-5"></div>
     <div class="row">
         <div class="col-md-12">
             <h3 class="text-center fw-bold">Image Slide</h3>
@@ -16,5 +16,35 @@
         </ol>
     </nav>
     <hr>
-    
+    <div class="row">
+        <div class="col-md-12">
+            <button data-bs-toggle="modal" data-bs-target="#addModal" class="btn btn-success"><i
+                    class="bi bi-plus-circle me-2"></i>ເພີ່ມ</button>
+            <div class="modal fade" id="addModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">ເພີ່ມຂໍ້ມູນຮູບແບບປະກັນໄພ</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <form action="" method="post" enctype="multipart/form-data">
+                            <div class="modal-body">
+                               <div class="mb-3">
+                                    <input type="file" name="image_path" id="image_path" class="form-control-file form-control-file-log">
+                               </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal"><i
+                                        class="bi bi-x-circle"></i> ອອກ</button>
+                                <button type="button" class="btn btn-success"><i class="bi bi-check-circle"></i>
+                                    ຕົກລົງ</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
